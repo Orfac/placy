6edac2b34f101ddcac383e8c906f774f033afd41
arseniy <molodeckij.b.2017@fml31.ru>
2020-03-21 04:41:38 +0300

    add support for params in categories

--- placy/src/main/kotlin/placy/api/Categories.kt
+++ placy/src/main/kotlin/placy/api/Categories.kt
@@ -6,19 +6,32 @@ import com.github.kittinunf.fuel.jackson.responseObject
 import com.github.kittinunf.result.Result;
 
 import placy.dto.Category
+import java.lang.StringBuilder
 
-class Categories {
-  val URL = "${Config.KUDAGO_URL}/place-categories/"
-  fun getCategories() : Array<Category>{
-    val (_,_,result) = Fuel.get(URL).responseObject<Array<Category>>()
+object Categories {
+  val CATEGORIES_URL = "${Config.KUDAGO_URL}/place-categories/?"
+  var language: String = ""
+  var fields: Array<String> = emptyArray()
+  var orderBy: String = ""
+
+  fun getCategories(): Array<Category> {
+    val requestUrl = buildURL()
+    val (_, _, result) = Fuel.get(requestUrl).responseObject<Array<Category>>()
+
     when (result) {
       is Result.Failure -> {
-        throw Exception("Cannot get categories from remote url ${URL}")
+        throw Exception("Cannot get categories from remote url $requestUrl")
       }
       is Result.Success -> {
         return result.value
       }
     }
+  }
 
+  private fun buildURL(): String {
+    val languageArgument = Utils.createArgument(language, "lang")
+    val fieldsArgument = Utils.createArraySingleArgument(fields, "fields")
+    val orderByFieldsArgument = Utils.createArgument(orderBy, "order_by")
+    return CATEGORIES_URL + languageArgument + fieldsArgument + orderByFieldsArgument
   }
 }
---
+++ placy/src/main/kotlin/placy/api/Utils.kt
@@ -0,0 +1,15 @@
+package placy.api
+
+internal class Utils {
+  internal companion object {
+    fun createArraySingleArgument(argument : Array<String>, argumentName : String) : String{
+      return if (argument.isEmpty()) ""
+      else "${argumentName}=${argument.joinToString(",")}&"
+    }
+
+    fun createArgument(argument : String, argumentName : String) : String{
+      return if (argument.isBlank()) ""
+      else "${argumentName}=${argument}&"
+    }
+  }
+}
