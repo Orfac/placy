0d0628882baf976f49f6f5d96a6b7e39b5a29999
Molodetskiy Arseniy <molodeckij.b.2017@fml31.ru>
2020-06-19 21:40:46 +0300

    add floating action button

--- tracy/app/src/main/java/com/example/tracy/MapsActivity.kt
+++ tracy/app/src/main/java/com/example/tracy/MapsActivity.kt
@@ -2,43 +2,70 @@ package com.example.tracy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.ArrayList
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var points : ArrayList<LatLng>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
+        points = ArrayList<LatLng>()
+        // Add a marker in Saint Petersburg
         val saintPetersburgLocation = LatLng(59.932473, 30.349142)
-        mMap.addMarker(MarkerOptions().position(saintPetersburgLocation).title("It is saint P"))
+        mMap.addMarker(MarkerOptions().position(saintPetersburgLocation).title("location is saint P"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(saintPetersburgLocation))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(saintPetersburgLocation, 12f))
 
+        mMap.setOnMapLongClickListener {
+            onClick(it)
+        }
     }
+
+    private fun onClick(location: LatLng) {
+        if (points.size == 2) {
+            points.clear()
+            mMap.clear()
+        }
+
+        points.add(location)
+        val marker = MarkerOptions()
+        marker.position(location)
+
+        if (points.size == 1) {
+            val greenPointer =
+                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+            marker.icon(greenPointer)
+        }
+        mMap.addMarker(marker)
+
+        if (points.size == 2){
+                val url = getRequestUrl(points[0],points[1])
+            }
+    }
+
+    private fun getRequestUrl(origin: LatLng, destination: LatLng): String {
+            val originString = "origin=" + origin.latitude + ","+ origin.longitude
+            return originString
+    }
+
+    fun doContent(view: View) {}
 }
